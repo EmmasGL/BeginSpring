cabb8c2decfa539d5d8c3068323819b40eb0d186
Emmanuel <119020398+EmmasGL@users.noreply.github.com>
2025-05-26 18:31:07 -0600

    render
    Pasar datos a nuestra vista

--- spring-boot-web/src/main/java/com/curso/springboot/spring_boot_web/controllers/HomeControllers.java
+++ spring-boot-web/src/main/java/com/curso/springboot/spring_boot_web/controllers/HomeControllers.java
@@ -1,9 +1,14 @@
 package com.curso.springboot.spring_boot_web.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeControllers {
@@ -13,7 +18,18 @@ public class HomeControllers {
 	//ESte ejemplo es mas corto y tambien el metodo puede ser mapeado a varias rutas 
 	//@GetMapping(value = "/index")
 	@GetMapping({"/index", "/", "home"})
-	public String index() {
-		return "index";
+	
+	//Para pasar datos existen 4 formas basicas la cual es con model, ModelMap, Map y ModelAndView(los 4 trabajan con llave:valor )
+	//* Model es el que mas se utiliza
+	//public String index(Model model) {
+	//public String index(ModelMap model) {
+	//public String index(Map<String, Object> map) {
+	public ModelAndView index(ModelAndView mv) {
+		//model.addAttribute("titulo", "Hola Spring Framework! jiji");// model y ModelMap
+		//map.put("titulo", "Esto es una prueba con Map");  // Map
+		mv.addObject("titulo", "Hola Spring Framework! jijiaaaa");  //ModelAndView
+		//return "index"; // model, ModelMap, Map
+		mv.setViewName("index");
+		return mv;
 	}
 }
